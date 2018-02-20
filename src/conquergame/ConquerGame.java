00863144efaa32cf979d1198d6cc9b782a4ffaef
Timo Polvinen <timo.polvinen@gmail.com>
2018-02-20 10:48:33 +0200

    Base for game operations

--- src/conquergame/ConquerGame.java
+++ src/conquergame/ConquerGame.java
@@ -5,6 +5,8 @@
  */
 package conquergame;
 
+import java.util.Scanner;
+
 /**
  *
  * @author emcq
@@ -16,6 +18,28 @@ public class ConquerGame {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner reader = new Scanner(System.in);
+        Player mainChar = new Player();
+        
+        while (true){
+            System.out.println("Press 1 to become more powerful!"
+                    + "You win when your power is at 100.");
+            
+            String command = reader.nextLine();
+            if (command.equals("1")){
+                System.out.println("POWER OVERWHELMING!");
+                mainChar.increasePower();
+            } else {
+                System.out.println("No power for me, thanks.");
+            }
+            System.out.println("My power level is at "+ mainChar.getPower());
+            if (mainChar.getPower() == 100){
+                break;
+            }
+        }
+        
+        System.out.println("You win!");
+        
     }
     
 }
